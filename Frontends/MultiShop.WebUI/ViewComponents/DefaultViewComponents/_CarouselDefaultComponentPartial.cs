bd830eecf17f9f4aa6dcf77a70ed14ed32deddf9
Hüseyin Çiçek <mrcicek.huseyin@gmail.com>
2024-12-13 22:30:15 +0300

    MultiShop.WebUI projesinde eklediğimiz template deki anasayfa tasarımını component yapısını kullanarak partial lara böldüm. Ve anasayfa tasarımı bitti.

---
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_CarouselDefaultComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _CarouselDefaultComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
