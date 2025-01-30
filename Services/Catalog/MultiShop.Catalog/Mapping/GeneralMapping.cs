0240410ace5a7fb35d55876ce7eda72ef1684e0c
Hüseyin Çiçek <mrcicek.huseyin@gmail.com>
2025-01-30 16:13:25 +0300

    Öne çıkan ürünler için Feature Slider tablosu oluşturdum. Backend tarafında işlemleri tamamladım. Frontend tarafında ise admin tarafında dizaynı yaptım ve api dan gelen verileri çekerek işlemleri yaptım. Backend ve admin tarafında ki işlemler bitti.

--- Services/Catalog/MultiShop.Catalog/Mapping/GeneralMapping.cs
+++ Services/Catalog/MultiShop.Catalog/Mapping/GeneralMapping.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using MultiShop.Catalog.Dtos.CategoryDtos;
+using MultiShop.Catalog.Dtos.FeatureSliderDtos;
 using MultiShop.Catalog.Dtos.ProductDetailDtos;
 using MultiShop.Catalog.Dtos.ProductDtos;
 using MultiShop.Catalog.Dtos.ProductImageDtos;
@@ -33,6 +34,11 @@ namespace MultiShop.Catalog.Mapping
             CreateMap<ProductImage, GetByIdProductImageDto>().ReverseMap();
 
             CreateMap<Product,ResultProductsWithCategoryDto>().ReverseMap();
+
+            CreateMap<FeatureSlider,ResultFeatureSliderDto>().ReverseMap();
+            CreateMap<FeatureSlider,CreateFeatureSliderDto>().ReverseMap();
+            CreateMap<FeatureSlider,UpdateFeatureSliderDto>().ReverseMap();
+            CreateMap<FeatureSlider,GetByIdFeatureSliderDto>().ReverseMap();
         }
     }
 }
