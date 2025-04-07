630df6608cebf2baaeefc690dcdc858403a901f9
Hüseyin Çiçek <mrcicek.huseyin@gmail.com>
2025-04-08 00:59:16 +0300

    Ana sayfada ki ürün kategorileri dinamik bir şekilde geliyor. Ürün kategorisine göre ürünler geliyor.

--- Frontends/MultiShop.WebUI/ViewComponents/UILayoutViewComponents/_NavbarUILayoutComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/UILayoutViewComponents/_NavbarUILayoutComponentPartial.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.CategoryDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.UILayoutViewComponents
 {
     public class _NavbarUILayoutComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _NavbarUILayoutComponentPartial(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
         }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7072/api/Categories");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                //Gelen veriyi string formatta oku.
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }   
     }
 }
