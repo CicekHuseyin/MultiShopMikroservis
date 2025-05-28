8f8ee6fbce2bd6f46acbd59d44d12ab6c9f469b1
Hüseyin Çiçek <mrcicek.huseyin@gmail.com>
2025-05-28 15:37:53 +0300

    Ürün detay sayfası tasarlandı üst kısımda ki birkaç ürün bilgisi admin panelinden dinamik olarak geliyor.

--- Services/Catalog/MultiShop.Catalog/Services/ProductImageServices/IProductImageService.cs
+++ Services/Catalog/MultiShop.Catalog/Services/ProductImageServices/IProductImageService.cs
@@ -10,5 +10,6 @@ namespace MultiShop.Catalog.Services.ProductImageServices
         Task UpdateProductImageAsync(UpdateProductImageDto updateProductImageDto);
         Task DeleteProductImageAsync(string id);
         Task<GetByIdProductImageDto> GetByIdProductImageAsync(string id);
+        Task<GetByIdProductImageDto> GetByProductIdProductImageAsync(string id);
     }
 }
